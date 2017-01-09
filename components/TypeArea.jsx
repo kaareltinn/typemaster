39d0b588274a3b576cc9c9654796c6404c535079
Kaarel Tinn <kaareltinn@hotmail.com>
2017-01-09 13:50:59 +0200

    TypeArea handle input

--- components/TypeArea.jsx
+++ components/TypeArea.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 
 export var TypeArea = React.createClass({
+  getInitialState: function() {
+    return { wordsTyped: '' }
+  },
+
+  handleTyping: function(e) {
+    this.setState({wordsTyped: e.target.value});
+  },
+
   render: function() {
     return (
       <div>
-        <input type="text" />
+        <input
+          type="text"
+          value={this.state.wordsTyped}
+          onChange={this.handleTyping}
+        />
       </div>
     );
   }
